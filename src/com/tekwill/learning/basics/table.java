48b18f11bc40d4297d3e32f2a0e5e702fc1e8ab9
deadmoroz369 <a.gorceag@gmail.com>
2020-11-30 09:45:44 -0800

    homework_all

---
+++ src/com/tekwill/learning/basics/table.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class table {
+    public static void main (String [] arg){
+        System.out.println("\t\t(\"X                        X\");\n" +
+                           "\t\t(\"X                        X\");\t                                          \n" +
+                       "\t\t(\"X       XXXXXXXXXX       X\");\n" +
+                       "\t\t(\"XXXXX   X        X   XXXXX\");\n" +
+                       "\t\t(\"X   X   X        X   X   X\");\n" +
+                       "\t\t(\"X   X   X        X   X   X\");");
+    }
+}
