fdd2596d4aac161f7d41b12f0373fbea211f5d23
deadmoroz369 <a.gorceag@gmail.com>
2020-11-30 09:58:58 -0800

    add HelloPeople

---
+++ src/com/tekwill/learning/basics/HelloPeople.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.basics;
+
+public class HelloPeople {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mike!\nHello,James!\nHello Charles!");
+    }
+}
