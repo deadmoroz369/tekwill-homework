5d5a6961dc6d926b0ca950edfd6d9398120e7261
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:44:12 -0800

    MISP

--- src/com/tekwill/learning/basics/TheBigWord.java
+++ src/com/tekwill/learning/basics/TheBigWord.java
@@ -2,6 +2,6 @@ package com.tekwill.learning.basics;
 
 public class TheBigWord {
     public static void main(String[] args) {
-        System.out.println(new StringBuilder().append("M     M      IIIII       SSSSS      PPPPPP  \n").append("MM   MM        I        S     S     P     P\n").append("M M M M        I        S           P     P\n").append("M  M  M        I         SSSSS      PPPPPP\n").append("M     M        I              S     P\n").append("M     M        I        S     S     P\n").append("M     M      IIIII       SSSSS      P").toString());
+        System.out.println("M     M      IIIII       SSSSS      PPPPPP  \n" + "MM   MM        I        S     S     P     P\n" + "M M M M        I        S           P     P\n" + "M  M  M        I         SSSSS      PPPPPP\n" + "M     M        I              S     P\n" + "M     M        I        S     S     P\n" + "M     M      IIIII       SSSSS      P");
     }
 }
