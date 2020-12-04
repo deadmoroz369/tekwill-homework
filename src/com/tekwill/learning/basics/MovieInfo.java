3b23cb826f3d96e8ab23c4c15e6d6d529d0f1b97
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:36:33 -0800

    Where does the favorite quote comes from?

--- src/com/tekwill/learning/basics/MovieInfo.java
+++ src/com/tekwill/learning/basics/MovieInfo.java
@@ -1,6 +1,6 @@
 package com.tekwill.learning.basics;
 
-public class MovieInfo1 {
+public class MovieInfo {
     public static void main(String[] args) {
         System.out.println("In death, I'm alone. And my only judge is me. \n(Adam) \n[Dark TV Series(2017)]");
     }
