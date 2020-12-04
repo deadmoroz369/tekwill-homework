8e29db116afdccc26d98874e1c6e6238e432bbad
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:31:27 -0800

    A very bright lamp made just of stars (or asterixes)

--- src/com/tekwill/learning/basics/TheStarLamp.java
+++ src/com/tekwill/learning/basics/TheStarLamp.java
@@ -1,6 +1,6 @@
 package com.tekwill.learning.basics;
 
-public class TheLamp {
+public class TheStarLamp {
     public static void main(String[] args) {
         System.out.print(new StringBuilder().append("    *****\n").append("  *********\n").append("*************\n").append("\n").append("    *****\n").append("  *********\n").append("*************\n").append("* | | | | | *\n").append("*************\n").append("    *****\n").append("  *********\n").append("*************\n").append("\n").append("    *****\n").append("  *********\n").append("*************\n").append("    *****\n").append("* | | | | | *\n").append("* | | | | | *\n").append("    *****\n").append("    *****\n").toString());
     }
