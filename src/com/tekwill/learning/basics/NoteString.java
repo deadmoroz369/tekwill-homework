ea2149d8224c94320503d0767d210d3319c6fdfd
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:42:15 -0800

    A note about Strings

--- src/com/tekwill/learning/basics/NoteString.java
+++ src/com/tekwill/learning/basics/NoteString.java
@@ -2,8 +2,7 @@ package com.tekwill.learning.basics;
 
 public class NoteString {
     public static void main(String[] args) {
-        System.out.println("A " + "\"quoted\"" + " String is\n'much' better if you learn\n the rules of \"escape sequences.\"");
-        System.out.println("Also, \"\" represents an empty String.\nDon't forget: use " + "\\\"" + "\" !\n" + "\'\' is not the same as \"");
+    System.out.println("A \"quoted\" String is\n" + "'much' better if you learn\n" + "the rules of \"escape sequences.\"\n" + "Also, \"\" represents an empty String.\n" + "Don't forget: use \\\"\" !\n" + "'' is not the same as \"\n");
     }
 }
 
