fb62329b7dba230c9fd8d5cc2b327b20498fff98
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:45:50 -0800

    A very bright lamp made of stars (or asterixes)

--- src/com/tekwill/learning/basics/TheStarLamp.java
+++ src/com/tekwill/learning/basics/TheStarLamp.java
@@ -2,6 +2,6 @@ package com.tekwill.learning.basics;
 
 public class TheStarLamp {
     public static void main(String[] args) {
-        System.out.print(new StringBuilder().append("    *****\n").append("  *********\n").append("*************\n").append("\n").append("    *****\n").append("  *********\n").append("*************\n").append("* | | | | | *\n").append("*************\n").append("    *****\n").append("  *********\n").append("*************\n").append("\n").append("    *****\n").append("  *********\n").append("*************\n").append("    *****\n").append("* | | | | | *\n").append("* | | | | | *\n").append("    *****\n").append("    *****\n").toString());
+        System.out.print("    *****\n" + "  *********\n" + "*************\n" + "\n" + "    *****\n" + "  *********\n" + "*************\n" + "* | | | | | *\n" + "*************\n" + "    *****\n" + "  *********\n" + "*************\n" + "\n" + "    *****\n" + "  *********\n" + "*************\n" + "    *****\n" + "* | | | | | *\n" + "* | | | | | *\n" + "    *****\n" + "    *****\n");
     }
 }
