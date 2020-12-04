9dea3968139cd3f4539ec3d08a522c6f2b1aaa63
deadmoroz369 <a.gorceag@gmail.com>
2020-12-03 23:34:33 -0800

    My favourite movie quote!

--- src/com/tekwill/learning/basics/MovieQuote1.java
+++ src/com/tekwill/learning/basics/MovieQuote1.java
@@ -1,7 +1,7 @@
 package com.tekwill.learning.basics;
 
-public class MovieQuote {
-    public static void main (String [] args){
+public class MovieQuote1 {
+    public static void main(String[] args) {
         System.out.println("In death, I'm alone. And my only judge is me.");
     }
 }
