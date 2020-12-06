f88ecb9b39ba8c7dd2bac8bd7610ec28fd53e534
deadmoroz369 <a.gorceag@gmail.com>
2020-12-06 10:48:27 -0800

    Documentation

---
+++ src/com/tekwill/learning/basics/package_modifiers/testing/Documentation.java
@@ -0,0 +1,28 @@
+package com.tekwill.learning.basics.package_modifiers.testing;
+
+import com.tekwill.learning.basics.package_modifiers.software.Employee;
+
+public class Documentation extends Employee {
+    private Employee employee;
+
+    public void checkPublic() {
+        employee.publicFirstName = "value";
+        employee.public_doWork();
+    }
+
+    public void checkProtected() {
+//        employee.protectedFirstName = "value";
+//        employee.protected_doWork(); [NO ACCESS]
+    }
+
+    public void checkDefault() {
+//        employee.defaultFirstName = "value";
+//        employee.default_doWork(); [NO ACCESS]
+    }
+
+    public void checkPrivate() {
+//        employee.privateFirstName = "value";
+//        employee.private_doWork();
+    }
+
+}
