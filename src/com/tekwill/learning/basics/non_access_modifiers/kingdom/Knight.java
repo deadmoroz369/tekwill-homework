7514fc41a51657eefbe4d32d1abf188e75a3b5ce
deadmoroz369 <a.gorceag@gmail.com>
2020-12-09 02:04:13 -0800

    My White Knight!

---
+++ src/com/tekwill/learning/basics/non_access_modifiers/kingdom/Knight.java
@@ -0,0 +1,32 @@
+package com.tekwill.learning.basics.non_access_modifiers.kingdom;
+
+public class Knight {
+
+    static String guild = "Hunters";
+    private final int age;
+    private final String nickname;
+
+
+    public Knight(int age, String nickname) {
+        this.age = age;
+        this.nickname = nickname;
+    }
+
+    public static char invokeMagic() {
+        System.out.println("Maaaagic");
+        return 0;
+    }
+
+    public char fight() {
+        System.out.println("Fight");
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "The knight: " + nickname + " is " + age + " years old!\n";
+
+
+    }
+}
+
---
+++ src/com/tekwill/learning/basics/non_access_modifiers/kingdom/KnightDemo.java
@@ -0,0 +1,15 @@
+package com.tekwill.learning.basics.non_access_modifiers.kingdom;
+
+public class KnightDemo {
+    public static void main(String[] args) {
+        Knight k1 = new Knight(33, "Johnson");
+        Knight k2 = new Knight(19, "Fred");
+
+        System.out.println(k1);
+        System.out.println(k2);
+        System.out.println(k1.fight());
+        System.out.println(Knight.invokeMagic());
+    }
+
+
+}
