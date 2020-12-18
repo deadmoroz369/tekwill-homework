c55531a773897f46e46891335e1f1bbe9f7edd0f
deadmoroz369 <a.gorceag@gmail.com>
2020-12-18 02:50:21 -0800

    Coordinates

---
+++ src/com/tekwill/learning/datatypes/io/reading/Coordinates.java
@@ -0,0 +1,30 @@
+package com.tekwill.learning.datatypes.io.reading;
+
+import java.util.Scanner;
+
+public class Coordinates {
+
+    public static void main(String[] args) {
+
+        double latitude;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Let's find out your location!\n" +
+                "First enter the degrees: ");
+        double degrees = scanner.nextDouble();
+
+        System.out.print("Now, enter the minutes of arc: ");
+        double minutes = scanner.nextDouble();
+
+        System.out.print("Finally, enter the seconds of arc: ");
+        double seconds = scanner.nextDouble();
+
+        latitude = degrees + minutes / 60 + seconds / 3600;
+
+        System.out.printf(degrees + " degrees, " + minutes + " minutes, " + seconds + " seconds = %.4f%n", latitude);
+
+
+    }
+}
+
+
