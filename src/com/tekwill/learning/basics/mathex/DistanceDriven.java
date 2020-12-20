4875e4811ab9cf67c0e9e04045eac0581ef91352
deadmoroz369 <a.gorceag@gmail.com>
2020-12-19 19:46:35 -0800

    Distance Driven (U.S. vs Europe)

--- src/com/tekwill/learning/basics/mathex/DistanceDriven.java
+++ src/com/tekwill/learning/basics/mathex/DistanceDriven.java
@@ -31,24 +31,5 @@ public class DistanceDriven {
     }
 
 
-
-
-/* public static void main(String[] args) {
-        System.out.println("Let's do a " + TOPIC_1 + "\nPress Enter to Continue!");
-        Scanner scanner = new Scanner(System.in);
-        String enterFromKeyboard = scanner.nextLine();
-        System.out.println("Question #1:\n" + LEVEL_1_QUESTION_1_TOPIC_1);
-        System.out.println("a)" + LEVEL_1_Q1_ANSWER_1_TOPIC_1 + "\n" + "b)" + LEVEL_1_Q1_ANSWER_2_TOPIC_1 + "\n" + "c)" + LEVEL_1_Q1_ANSWER_3_TOPIC_1 + "\n" + "d)" + LEVEL_1_Q1_CORRECT_ANSWER_TOPIC_1);
-        String stringYourAnswer = scanner.nextLine();
-        if (stringYourAnswer.equals(LEVEL_1_Q1_SHORT_ANSWER_D_TOPIC_1)) {
-            System.out.println("Correct!");
-    1. Ask a prompt for how many miles you drove and how many gallons you have used
-   2. Having the prompts will calculate and print how many MPG the car's driven!
-   3. If you prefer can make the EU style where Liters and km are used!
-   4. OR you can use if else statement if you like --> the prompt asks which style you want and depending on that prints out
-
- */
-
-
 }
 
---
+++ src/com/tekwill/learning/quiz/Game.java
@@ -0,0 +1,4 @@
+package com.tekwill.learning.quiz;
+
+public class Game {
+}
