b6b295244cd2afd84d75292103e67f79fcd0b9a2
deadmoroz369 <a.gorceag@gmail.com>
2020-12-19 19:52:30 -0800

    Liter/km to MPG

---
+++ src/com/tekwill/learning/basics/mathex/LPK_to_MPG.java
@@ -0,0 +1,25 @@
+package com.tekwill.learning.basics.mathex;
+
+import java.util.Scanner;
+
+public class LPK_to_MPG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter fuel in liters:");
+        float siUnitLiquid = scanner.nextFloat();
+        System.out.println("Now, enter distance in km:");
+        int siUnitDistance = scanner.nextInt();
+        float L_TO_G = siUnitLiquid / 3.785F;
+        float KM_TO_MI = siUnitDistance * 0.621F;
+        float US_FORMULA = KM_TO_MI / L_TO_G;
+
+        System.out.println("The European style uses L/km while US style uses Miles per Gallon or MPG.\nYou entered: " + siUnitDistance + " km and " + siUnitLiquid + " L\nConverting that to MPG style will result: ");
+        System.out.printf("%.1f%s", US_FORMULA, " MPG");
+
+    }
+
+
+}
+
+
+
