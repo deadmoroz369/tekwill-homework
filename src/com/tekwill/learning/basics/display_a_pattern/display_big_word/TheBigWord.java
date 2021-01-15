85a54c5693a94728c7a1c69f2914bb6df5918471
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:40:41 -0800

    (2 Dec) Display a Big Word "MISP"

--- src/com/tekwill/learning/basics/display_a_pattern/display_big_word/TheBigWord.java
+++ src/com/tekwill/learning/basics/display_a_pattern/display_big_word/TheBigWord.java
@@ -1,5 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.display_big_word;
-
+//
 public class TheBigWord {
     public static void main(String[] args) {
         System.out.println("M     M      IIIII       SSSSS      PPPPPP  \n" + "MM   MM        I        S     S     P     P\n" + "M M M M        I        S           P     P\n" + "M  M  M        I         SSSSS      PPPPPP\n" + "M     M        I              S     P\n" + "M     M        I        S     S     P\n" + "M     M      IIIII       SSSSS      P");
