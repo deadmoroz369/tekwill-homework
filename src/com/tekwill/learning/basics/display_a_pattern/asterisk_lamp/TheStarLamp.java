19813c628eaeb3f48c219d92305b3fca6f94dd05
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:37:37 -0800

    (2 Dec) Display a Lamp of Asterisks

--- src/com/tekwill/learning/basics/display_a_pattern/asterisk_lamp/TheStarLamp.java
+++ src/com/tekwill/learning/basics/display_a_pattern/asterisk_lamp/TheStarLamp.java
@@ -1,5 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.asterisk_lamp;
-
+//
 public class TheStarLamp {
     public static void main(String[] args) {
         System.out.print("    *****\n" + "  *********\n" + "*************\n" + "\n" + "    *****\n" + "  *********\n" + "*************\n" + "* | | | | | *\n" + "*************\n" + "    *****\n" + "  *********\n" + "*************\n" + "\n" + "    *****\n" + "  *********\n" + "*************\n" + "    *****\n" + "* | | | | | *\n" + "* | | | | | *\n" + "    *****\n" + "    *****\n");
