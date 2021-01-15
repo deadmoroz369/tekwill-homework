7ca373244575e329127be20a29b09d57eaffa6ff
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:44:42 -0800

    (27 Nov) Display "Hello!" to 3 people

--- src/com/tekwill/learning/basics/display_a_pattern/hello_people/HelloPeople.java
+++ src/com/tekwill/learning/basics/display_a_pattern/hello_people/HelloPeople.java
@@ -1,5 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.hello_people;
-
+//
 public class HelloPeople {
     public static void main(String[] args) {
         System.out.println("Hello, Mike!\nHello,James!\nHello Charles!");
