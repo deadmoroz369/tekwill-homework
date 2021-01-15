f2915a6668af27d357d06fab36c981f25d81deb5
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:11:11 -0800

    Display the quote from Your Beloved TV show (27 Nov)

--- src/com/tekwill/learning/basics/display_a_pattern/movie_quote/MovieQuote.java
+++ src/com/tekwill/learning/basics/display_a_pattern/movie_quote/MovieQuote.java
@@ -1,6 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.movie_quote;
 
-//display a pattern
 public class MovieQuote {
     public static void main(String[] args) {
         System.out.println("In death, I'm alone. And my only judge is me.");
