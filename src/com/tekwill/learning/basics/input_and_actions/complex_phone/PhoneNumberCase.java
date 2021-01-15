8ec7171dfde50a7b770d72292349024533dff8d9
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:59:46 -0800

    (30 Dec) Input a word and get the phone number

--- src/com/tekwill/learning/basics/input_and_actions/complex_phone/PhoneNumberCase.java
+++ src/com/tekwill/learning/basics/input_and_actions/complex_phone/PhoneNumberCase.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import static com.tekwill.learning.basics.input_and_actions.complex_phone.PhoneLettersToKeys.*;
 
-
 public class PhoneNumberCase {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
