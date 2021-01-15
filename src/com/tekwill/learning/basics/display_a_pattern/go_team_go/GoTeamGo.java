736ca0ba140377cbcbb3cae8378ecd6e447efcc3
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 04:32:02 -0800

    A "Very" Motivational Shout (2 Dec)

--- src/com/tekwill/learning/basics/display_a_pattern/go_team_go/GoTeamGo.java
+++ src/com/tekwill/learning/basics/display_a_pattern/go_team_go/GoTeamGo.java
@@ -1,6 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.go_team_go;
 
-//display a pattern
 public class GoTeamGo {
     public static void main(String[] args) {
         System.out.println("Go, team, go!\n" + "You can do it.\n" + "\n" + "Go, team, go!\n" + "You can do it.\n" + "You're the best,\n" + "In the West.\n" + "Go, team, go!\n" + "You can do it.\n" + "\n" + "Go, team, go!\n" + "You can do it. ");
