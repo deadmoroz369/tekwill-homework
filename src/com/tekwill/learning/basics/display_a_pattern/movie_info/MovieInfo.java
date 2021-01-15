56f61774a36af46be91ddfc35de9a841694c2634
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:48:18 -0800

    (27 Nov) Display your Beloved TV show

--- src/com/tekwill/learning/basics/display_a_pattern/movie_info/MovieInfo.java
+++ src/com/tekwill/learning/basics/display_a_pattern/movie_info/MovieInfo.java
@@ -1,5 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.movie_info;
-
+//
 public class MovieInfo {
     public static void main(String[] args) {
         System.out.println("In death, I'm alone. And my only judge is me. \n(Adam) \n[Dark TV Series(2017)]");
