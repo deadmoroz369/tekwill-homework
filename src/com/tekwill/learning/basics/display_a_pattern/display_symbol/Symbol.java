e3a5ba9a6329358d23273816cfac38852ce509e2
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 04:29:16 -0800

    Display Symbol (30 Nov)

--- src/com/tekwill/learning/basics/display_a_pattern/display_symbol/Symbol.java
+++ src/com/tekwill/learning/basics/display_a_pattern/display_symbol/Symbol.java
@@ -1,6 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.display_symbol;
 
-//display a pattern 5
 public class Symbol {
     public static void main(String[] args) {
         System.out.println(" \t\\/\n   \\\\//\n  \\\\\\///\n  ///\\\\\\\n   \\\\//\n    /\\");
