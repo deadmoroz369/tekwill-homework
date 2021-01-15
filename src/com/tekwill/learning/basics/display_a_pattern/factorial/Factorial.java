0c71efd6fae3570402ffcf4c06f6d282116c410d
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 06:42:40 -0800

    (6 Jan) Display the Factorial

--- src/com/tekwill/learning/basics/display_a_pattern/factorial/Factorial.java
+++ src/com/tekwill/learning/basics/display_a_pattern/factorial/Factorial.java
@@ -1,5 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.factorial;
-
+//
 public class Factorial {
     public static void main(String[] args) {
         System.out.println("Number   Factorial");
