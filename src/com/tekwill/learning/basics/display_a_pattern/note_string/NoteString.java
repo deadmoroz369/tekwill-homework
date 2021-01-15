87b767f2961081a741c01cceb5a7e1fd96dee129
deadmoroz369 <a.gorceag@gmail.com>
2021-01-15 04:27:00 -0800

    Display Text (30 Nov)

--- src/com/tekwill/learning/basics/display_a_pattern/note_string/NoteString.java
+++ src/com/tekwill/learning/basics/display_a_pattern/note_string/NoteString.java
@@ -1,6 +1,5 @@
 package com.tekwill.learning.basics.display_a_pattern.note_string;
 
-//display a pattern
 public class NoteString {
     public static void main(String[] args) {
         System.out.println("A \"quoted\" String is\n" + "'much' better if you learn\n" + "the rules of \"escape sequences.\"\n" + "Also, \"\" represents an empty String.\n" + "Don't forget: use \\\"\" !\n" + "'' is not the same as \"\n");
