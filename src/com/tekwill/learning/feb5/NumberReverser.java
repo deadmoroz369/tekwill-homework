1a5e0a022260e9649255e515ad17de6274fba7d3
deadmoroz369 <a.gorceag@gmail.com>
2021-02-08 08:10:50 -0800

    Homework Completed!

---
+++ src/com/tekwill/learning/feb5/NumberReverser.java
@@ -0,0 +1,26 @@
+package com.tekwill.learning.feb5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NumberReverser {
+
+    public static void reverseNumbers(int numberOfIntegers) {
+        List<Integer> numbersList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter " + numberOfIntegers + " numbers");
+        for (int i = 0; i < numberOfIntegers; i++) {
+            int enteredNumber = scanner.nextInt();
+            numbersList.add(enteredNumber);
+        }
+        System.out.println("Your list is ");
+        System.out.println(numbersList);
+
+        System.out.println("This numbers in reversed order");
+        for (int i = numbersList.size() - 1; i >= 0; i--) {
+            System.out.print(numbersList.get(i) + " ");
+        }
+    }
+
+}
---
+++ src/com/tekwill/learning/feb5/NumberReverserDemo.java
@@ -0,0 +1,14 @@
+package com.tekwill.learning.feb5;
+
+import java.util.Scanner;
+
+public class NumberReverserDemo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of the integers that will be read :");
+        int numberOfIntegers = scanner.nextInt();
+        NumberReverser.reverseNumbers(numberOfIntegers);
+
+    }
+}
